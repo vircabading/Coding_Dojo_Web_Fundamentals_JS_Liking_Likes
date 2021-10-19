4453e6946bb3d63356d7ed5dc85fb0148d4a45db
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-19 13:37:02 -0700

    add user feed

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 // By:      Virgilio D. Cabading Jr.
 //////////////////////////////////////////////////
 
+var userFeed = [
+    {"name" : "Neil M",
+    "likes" : 9,
+    "message" : "I just set up this new discord bot, let me know what you all think. <a href=''>link</a>"},
+    {"name" : "Nichole K",
+    "likes" : 12,
+    "message" : "Just finished my <a href=''>next comic</a>},
+    {"name" : "Jim",
+    "likes" : 9,
+    "message" : "I can't stop listening to the <a href="">new album.</a>  Who else is liking it?"},
+];
+
+function printUserFeed() {
+    
+}
